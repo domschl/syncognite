96c2bc82d91108f1a4fe8ef58d7638020a084483
Dominik Schloesser <dsc@dosc.net>
2016-08-22 09:23:55 +0200

    First optimizer (untested): SDG

--- cpneural/cp-optim.h
+++ cpneural/cp-optim.h
@@ -8,16 +8,41 @@ typedef map<string, floatN> parmap;
 class optimizer {
 public:
     parmap params;
-    optimizer(map<string, floatN>& ps) {
+        /*
+        optimizer(map<string, floatN>& ps) {
         for (parmap iterator = ps.begin; iterator != ps.end(); iterator++) {
             params[iterator->first]=iterator.second;
+            params=ps;
+            }
         }
+        */
+    floatN getPar(string par, floatN def) {
+        auto it=param.find[par];
+        if (it=ps.end()) {
+            param[par]=def;
+        }
+        return param[par];
     }
-    update(MatrixN& x, MatrixN& dx);
+    virtual update(MatrixN& x, MatrixN& dx) {return x;};
 }
 
+/*
+def sgd(w, dw, config=None):
+  if config is None: config = {}
+  config.setdefault('learning_rate', 1e-2)
+  w -= config['learning_rate'] * dw
+  return w, config
+*/
 class sdg : optimizer {
-
+    floatN lr;
+    sdg(parmap& ps) {
+        param=ps;
+        lr=getPar("learning_rate", 1e-2);
+    }
+    virtual update(MatrixN& x, MatrixN& dx) override {
+        x=x-lr*dx;
+        return x;
+    };
 }
 
 #endif
