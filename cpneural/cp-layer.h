5fcad990829f3825fd80ecee45283d947e7fd01b
Dominik Schloesser <dsc@dosc.net>
2016-09-06 17:02:27 +0200

    hacky parameter class added

--- cpneural/cp-layer.h
+++ cpneural/cp-layer.h
@@ -2,7 +2,9 @@
 #define _CP_LAYER_H
 
 #include <iostream>
+#include <cctype>
 #include <string>
+#include <algorithm>
 #include <sstream>
 #include <vector>
 #include <map>
@@ -60,29 +62,83 @@ public:
 
     }
 */
+    string trim(const string &s) {
+        auto wsfront=std::find_if_not(s.begin(),s.end(),[](int c){return std::isspace(c);});
+        //auto wsfront=std::find_if_not(s.begin(),s.end(),std::isspace);
+        auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
+        //auto wsback=std::find_if_not(s.rbegin(),s.end(),std::isspace).base();
+        return (wsback<=wsfront ? string() : string(wsfront,wsback));
+    }
     void split(const string &s, char delim, vector<string> &elems) {
         std::stringstream ss;
         ss.str(s);
         string item;
         while (getline(ss, item, delim)) {
-            elems.push_back(item);
+            elems.push_back(trim(item));
         }
     }
-    void setString(string jsonpars) {
-
+    string getBlock(string str, string del1, string del2) {
+        auto p1=str.find(del1);
+        auto p2=str.rfind(del2);
+        string r;
+        if (p2<=p1) r=string(); else r=str.substr(p1,p2-p1);
+        if (r.size()>=del1.size()+del2.size()) r=r.substr(del1.size(),str.size()-del1.size()-del2.size());
+        return r;
+    }
+    void setString(string str) {
+        string bl=getBlock(str,"{","}");
+        vector<string> tk;
+        split(bl,';',tk);
+        for (auto t : tk) {
+            vector<string> par;
+            string p1=getBlock(t,"","=");
+            string p2=getBlock(t,"=","");
+            if (p1.size()>0) {
+                if (p2[0]=='[') { // int array
+                    string tm=getBlock(p2,"[","]");
+                    vector<string> ar;
+                    split(tm,',',ar);
+                    vector<int> vi;
+                    for (auto ai : ar) {
+                        try {
+                            vi.push_back(stoi(ai));
+                        } catch (...) {}
+                    }
+                    setPar(p1,vi);
+                } else if (p2=="true") { // boolean
+                    setPar(p1,true);
+                } else if(p2=="false") { // boolean
+                    setPar(p1,false);
+                } else if (p2[0]=='\'') { //string
+                    // XXX: (de/)encode escape stuff:   ;{}
+                    setPar(p1,p2);
+                } else if (p2.find(".")!=p2.npos || p2.find("e")!=p2.npos) { //float
+                    try {
+                        setPar(p1,stof(p2));
+                    } catch (...) {}
+                } else { //assume int
+                    try {
+                        setPar(p1,stoi(p2));
+                    } catch (...) {}
+                }
+            }
+        }
     }
     string getString(bool pretty=true) {
-        string str="{";
-        string ind,sep,asep, ter;
+        string ind,sep,asep, ter, tnl;
+        string qt=""; // or: "\"";
+
         if (pretty) {
-            ind="  "; sep=": "; asep=", "; ter=";\n";
+            ind="  "; sep="="; asep=", "; ter=";\n"; tnl="\n";
         } else {
-            ind=""; sep=":"; asep=","; ter=";";
+            ind=""; sep="="; asep=","; ter=";"; tnl="";
         }
-        for (auto it : fparams) str += ind + "\""+it.first + "\""+sep + std::to_string(it.second) + ter;
-        for (auto it : iparams) str += ind + "\""+it.first + "\""+sep + std::to_string(it.second) + ter;
+        string sdl="'";
+        string str="{"+tnl;
+        for (auto it : fparams) str += ind + qt+it.first + qt+sep + std::to_string(it.second) + ter;
+        for (auto it : iparams) str += ind + qt+it.first + qt+sep + std::to_string(it.second) + ter;
         for (auto it : viparams) {
-            str += ind + "\""+it.first + "\""+sep + "[";
+            str += ind + qt+it.first + qt+sep + "[";
             bool is=false;
             for (auto i : it.second) {
                 if (is) str+=asep;
@@ -92,12 +148,16 @@ public:
             str+="]"+ter;
         }
         for (auto it : bparams) {
-            str += ind + "\""+it.first + "\""+sep;
+            str += ind + qt+it.first + qt+sep;
             if (it.second) str+="true";
             else str+="false";
             str+=ter;
         }
-        for (auto it : sparams) str += ind + "\""+it.first + "\""+sep +"\""+it.second +"\""+ ter;
+        for (auto it : sparams) {
+            // XXX: decode escape stuff:   ;{}
+            str += ind + qt+it.first + qt+sep +sdl+it.second +sdl+ ter;
+        }
+        str+="}"+tnl;
         return str;
     }
     bool isDefined(string par) {
@@ -105,6 +165,7 @@ public:
         if (viparams.find(par)!=viparams.end()) return true;
         if (fparams.find(par)!=fparams.end()) return true;
         if (bparams.find(par)!=bparams.end()) return true;
+        // XXX Warning: Strings lake even rudimentary escape sequence handling!
         if (sparams.find(par)!=sparams.end()) return true;
         return false;
     }
@@ -113,39 +174,41 @@ public:
         if (iparams.find(par)!=iparams.end()) iparams.erase(par);
         if (viparams.find(par)!=viparams.end()) viparams.erase(par);
         if (bparams.find(par)!=bparams.end()) bparams.erase(par);
+        // XXX Warning: Strings lake even rudimentary escape sequence handling!
         if (sparams.find(par)!=sparams.end()) sparams.erase(par);
     }
-    floatN getPar(string par, floatN def) {
+    floatN getPar(string par, floatN def=0.0) {
         auto it=fparams.find(par);
         if (it==fparams.end()) {
             fparams[par]=def;
         }
         return fparams[par];
     }
-    int getPar(string par, int def) {
+    int getPar(string par, int def=0) {
         auto it=iparams.find(par);
         if (it==iparams.end()) {
             iparams[par]=def;
         }
         return iparams[par];
     }
-    vector<int> getPar(string par, vector<int> def) {
+    vector<int> getPar(string par, vector<int> def={}) {
         auto it=viparams.find(par);
         if (it==viparams.end()) {
             viparams[par]=def;
         }
         return viparams[par];
     }
-    bool getPar(string par, bool def) {
+    bool getPar(string par, bool def=false) {
         auto it=bparams.find(par);
         if (it==bparams.end()) {
             bparams[par]=def;
         }
         return bparams[par];
     }
-    string getPar(string par, string def) {
+    string getPar(string par, string def="") {
         auto it=sparams.find(par);
         if (it==sparams.end()) {
+            // XXX: decode escape stuff:   ;{}
             sparams[par]=def;
         }
         return sparams[par];
@@ -168,9 +231,11 @@ public:
     }
     void setPar(string par, string val) {
         erase(par);
+        // XXX: encode escape stuff:   ;{}
         sparams[par]=val;
     }
 };
+
 
 class Optimizer : public CpParams {
 public:
