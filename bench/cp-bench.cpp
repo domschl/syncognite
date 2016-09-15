87f5c41bef0607e5fab10bf64b36375e2e6631d1
Dominik Schloesser <dsc@dosc.net>
2016-09-15 12:19:14 +0200

    simple layer-benchmarking

---
+++ bench/cp-bench.cpp
@@ -0,0 +1,105 @@
+#include "cp-neural.h"
+
+// Manual build:
+// g++ -g -ggdb -I ../cpneural -I /usr/local/include/eigen3 testneural.cpp -L ../Build/cpneural/ -lcpneural -lpthread -o test
+
+using std::cout; using std::endl;
+
+bool matComp(MatrixN& m0, MatrixN& m1, string msg="", floatN eps=1.e-6) {
+    if (m0.cols() != m1.cols() || m0.rows() != m1.rows()) {
+        cout << msg << ": Incompatible shapes " << shape(m0) << "!=" << shape(m1) << endl;
+        return false;
+    }
+    MatrixN d = m0 - m1;
+    floatN dif = d.cwiseProduct(d).sum();
+    if (dif < eps) {
+        cout << msg << " err=" << dif << endl;
+        return true;
+    } else {
+        IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+        cout << msg << " m0:" << endl << m0.format(CleanFmt) << endl;
+        cout << msg << " m1:" << endl << m1.format(CleanFmt) << endl;
+        cout << "err=" << dif << endl;
+        return false;
+    }
+}
+
+bool benchLayer(string name, Layer* player) {
+    MatrixN x(100,1000);
+    MatrixN y(100,1);
+    x.setRandom();
+    y.setRandom();
+    for (int i=0; i<y.size(); i++) {
+        y(i)=(floatN)(int)((y(i)+1)*5);
+    }
+    Timer tcpu;
+    double tcus;
+    t_cppl cache;
+    t_cppl grads;
+
+    tcpu.startCpu();
+    player->forward(x,nullptr);
+    tcus=tcpu.stopCpuMicro()/1000.0;
+    cout << name << " forward (no cache): " << tcus << "ms (cpu)." << endl;
+
+    tcpu.startCpu();
+    if (player->layerType==LayerType::LT_NORMAL) {
+        player->forward(x,&cache);
+    } else {
+        player->forward(x,y,&cache);
+    }
+    tcus=tcpu.stopCpuMicro()/1000.0;
+    cout << name << " forward (with cache): " << tcus << "ms (cpu)." << endl;
+
+    if (name=="BatchNorm") {
+        return false;
+    }
+
+    tcpu.startCpu();
+    if (player->layerType==LayerType::LT_NORMAL) {
+        player->backward(x,&cache, &grads);
+    } else {
+        player->backward(y,&cache, &grads);
+    }
+    tcus=tcpu.stopCpuMicro()/1000.0;
+    cout << name << " backward (with cache): " << tcus << "ms (cpu)." << endl;
+    cppl_delete(&cache);
+    cppl_delete(&grads);
+
+    return true;
+}
+
+int doBench() {
+    bool allOk=true;
+    Color::Modifier red(Color::FG_RED);
+    Color::Modifier green(Color::FG_GREEN);
+    Color::Modifier def(Color::FG_DEFAULT);
+    cout << "bench start" << endl;
+    int nr=0;
+    for (auto it : _syncogniteLayerFactory.mapl) {
+        ++nr;
+        cout << nr << ".: " << it.first << " ";
+        t_layer_props_entry te=_syncogniteLayerFactory.mapprops[it.first];
+        CpParams cp;
+        std::vector<int> tp(te);
+        for (auto i=0; i< tp.size(); i++) tp[i]=1000;
+        cp.setPar("topo",tp);
+        Layer *l = CREATE_LAYER(it.first, cp)
+        if (!benchLayer(it.first, l)) {
+            cout << "Error" << endl;
+            allOk=false;
+        }
+        delete l;
+    }
+    cout << "bench end, nr=" << nr << endl;
+    return allOk;
+}
+
+int main() {
+    Eigen::initParallel();
+    registerLayers();
+
+    int ret=0;
+    ret=doBench();
+    return ret;
+}
