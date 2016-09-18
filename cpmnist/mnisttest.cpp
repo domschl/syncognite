3d312c3f605a9582ea7f49045e4647ca8648b8da
Dominik Schloesser <dsc@dosc.net>
2016-09-18 12:32:11 +0200

    ongoing perf-threading

--- cpmnist/mnisttest.cpp
+++ cpmnist/mnisttest.cpp
@@ -225,7 +225,7 @@ bool  getMnistData(string filepath) {
 
     CpParams cpo("{verbose=true;learning_rate=1e-2;lr_decay=1.0;momentum=0.9;decay_rate=0.98;epsion=1e-8}");
     cpo.setPar("epochs",200);
-    cpo.setPar("batch_size",1000);
+    cpo.setPar("batch_size",2000);
     cpo.setPar("regularization", (floatN)0.0); //0.00001);
     floatN final_err;
 
