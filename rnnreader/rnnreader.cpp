ab47d3a75aec0bda8783b8d92cb3c69b3210981a
Dominik Schloesser <dsc@dosc.net>
2016-12-05 11:37:38 +0100

    stage

--- rnnreader/rnnreader.cpp
+++ rnnreader/rnnreader.cpp
@@ -298,10 +298,10 @@ int main(int argc, char *argv[]) {
                 }
                 //wcout << L"<" << endl;
 
-                //for (int t=T-1; t>0; t--) xg(0,t)=xg(0,t-1);
-                //xg(0,0)=xg2(0,0);
+                for (int t=T-1; t>0; t--) xg(0,t)=xg(0,t-1);
+                xg(0,0)=xg2(0,0);
 
-                xg=xg2;
+                //xg=xg2;
             }
             wcout << endl;
         }
