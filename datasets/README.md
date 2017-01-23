edb10ae871091fc8eae77e4dbb04403de9cffba1
Dominik Schloesser <dsc@dosc.net>
2017-01-23 16:40:00 +0100

    beautifications

--- datasets/README.md
+++ datasets/README.md
@@ -5,8 +5,8 @@ Downloads the original handwritten mnist digits from [Yan LeCun's website](http:
 The data is transformed into a single [HDF5 database](https://support.hdfgroup.org/HDF5/) file.
 
 ## Requirements:
-* HDF5 OS drivers (Arch linux: hdf5-cpp-fortran, Mac homebrew: )
-* Python HDF5 support: [python-h5py](http://docs.h5py.org/en/latest/index.html)
+* HDF5 OS drivers (Arch linux: ```hdf5-cpp-fortran```, Mac homebrew: ```hdf5```)
+* Python HDF5 support: [python-h5py](http://docs.h5py.org/en/latest/index.html), or ```pip install h5py```.
 
 ## Download required data:
 ```
--- datasets/download_mnist.py
+++ datasets/download_mnist.py
@@ -51,7 +51,7 @@ def loadOriginalFiles(datadict, destpath):
     for du in datadict:
         if not os.path.exists(destpath):
             print("Creating directory:", destpath)
-            os.makedirs(d)
+            os.makedirs(destpath)
         destfile = os.path.join(destpath, du["name"])
         if os.path.isfile(destfile) is True:
             if os.stat(destfile).st_size == du["size"]:
@@ -61,7 +61,7 @@ def loadOriginalFiles(datadict, destpath):
                 print(destfile, "local version is of wrong size, trying to re-download.")
         if firstDl is True:
             firstDl = False
-            print("Downloading original MNIST files from:", "http://yann.lecun.com/exdb/mnist/")
+            print("Downloading original MNIST files from: http://yann.lecun.com/exdb/mnist/")
             print("Reference: [LeCun et al., 1998a]")
             print("   Y. LeCun, L. Bottou, Y. Bengio, and P. Haffner. 'Gradient-based learning")
             print("   applied to document recognition.' Proceedings of the IEEE, 86(11):2278-2324,")
@@ -80,7 +80,7 @@ def loadOriginalFiles(datadict, destpath):
             print("Error: failed to write local file", destfile)
             return False
         if os.stat(destfile).st_size != du["size"]:
-            print("Error:", destfile, " was downloaded, but has unexpected size!")
+            print("Error:", destfile, " downloaded, unexpected size error!")
             return False
     return True
 
@@ -122,10 +122,11 @@ def encodeOriginalsToH5(datadict, zippath, h5path):
                         di[y, x] = float(data[offs+y*w+x]) / du["norm"]
             offs += du["datasets"][ds] * du["entrysize"]
             print(ds, shape, dtype)
-            dset = hf.create_dataset(ds, shape, dtype=dtype, compression='gzip')
+            dset = hf.create_dataset(ds, shape, dtype=dtype,
+                                     compression='gzip')
             dset[...] = di
     hf.close()
-
+    return True
 
 
 def checkFile():
@@ -136,11 +137,15 @@ def checkFile():
             np_data = np.array(data)
             print('Shape of the array ', ds, ': \n', np_data.shape)
             print(len(data))
-            print("0:",data[0])
+            print("0:", data[0])
 
 
-#
 localpath = os.path.dirname(os.path.realpath(__file__))
 destpath = os.path.join(localpath, "originals")
-loadOriginalFiles(mnist_urls, destpath)
-encodeOriginalsToH5(mnist_urls, destpath, localpath)
+if loadOriginalFiles(mnist_urls, destpath) is False:
+    print("Download failed.")
+else:
+    if encodeOriginalsToH5(mnist_urls, destpath, localpath) is True:
+        print("database file is now in:", destpath)
+    else:
+        print("Error during HDF5 file creation")
