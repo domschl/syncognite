1418bb6f72eaf16229c474eca44a2a6bd7ded928
Dominik Schloesser <dsc@dosc.net>
2017-02-10 10:22:11 +0100

    Bench for complete set of layers ok

--- bench/cp-bench.cpp
+++ bench/cp-bench.cpp
@@ -9,21 +9,26 @@ using std::cerr; using std::endl;
 using std::setprecision; using std::setw; using std::fixed;
 
 map<string, string> benchRecipes = {
-    {"Affine", "{benchN=100;inputShape=[1024];hidden=1024}"},
-    {"Relu", "{benchN=100;inputShape=[1024]}"},
-    {"Nonlinearity", "{benchN=100;inputShape=[1024]}"},
-    {"AffineRelu", "{benchN=100;inputShape=[1024];hidden=1024}"},
-    {"Convolution", "{benchN=100;inputShape=[3,32,32];kernel=[64,5,5];stride=1;pad=2}"},
-    {"Pooling", "{benchN=100;inputShape=[3,64,64];stride=2}"},
-    {"Dropout", "{benchN=100;inputShape=[1024];drop=0.5}"},
-    {"LSTM", "{benchN=100;inputShape=[100,80];N=100;H=256}"},
-    {"RNN", "{benchN=100;inputShape=[100,80];N=100;H=256}"},
-
-// BatchNorm, OneHot, Softmax, SpatialBatchNorm, Svm, TemporalAffine, TemporalSoftmax, TwoLayerNet, WordEmbedding
-//    {"Relu", "{inputShape=[1024]}"},
-//    {"Relu", "{inputShape=[1024]}"},
-//    {"Relu", "{inputShape=[1024]}"}
-
+    {"OneHot", "{benchIdx=0;benchName='OneHot';benchDataType='int100';benchN=100;V=100;inputShape=[100]}"},
+    {"Affine", "{benchIdx=1;benchName='Affine';benchN=100;inputShape=[1024];hidden=1024}"},
+    {"AffineRelu", "{benchIdx=2;benchName='AffineRelu';benchN=100;inputShape=[1024];hidden=1024}"},
+    {"Relu", "{benchIdx=3;benchName='Relu';benchN=100;inputShape=[1024]}"},
+    {"Nonlinearity0", "{benchIdx=4;benchName='Nonlin-Relu';benchN=100;type='relu';inputShape=[1024]}"},
+    {"Nonlinearity1", "{benchIdx=5;benchName='Nonlin-Sgmd';benchN=100;type='sigmoid';inputShape=[1024]}"},
+    {"Nonlinearity2", "{benchIdx=6;benchName='Nonlin-Tanh';benchN=100;type='tanh';inputShape=[1024]}"},
+    {"Dropout", "{benchIdx=7;benchName='Dropout';benchN=100;inputShape=[1024];drop=0.5}"},
+    {"BatchNorm", "{benchIdx=8;benchName='BatchNorm';benchN=100;inputShape=[1024]}"},
+    {"SpatialBatchNorm", "{benchIdx=9;benchName='SpatialBtchNrm';benchN=100;N=100;inputShape=[3,32,32]}"},
+    {"Convolution", "{benchIdx=10;benchName='Convolution';benchN=100;inputShape=[3,32,32];kernel=[64,5,5];stride=1;pad=2}"},
+    {"Pooling", "{benchIdx=11;benchName='Pooling';benchN=100;inputShape=[3,64,64];stride=2}"},
+    {"LSTM", "{benchIdx=12;benchName='LSTM';benchN=100;inputShape=[100,80];N=100;H=256}"},
+    {"RNN", "{benchIdx=13;benchName='RNN';benchN=100;inputShape=[100,80];N=100;H=256}"},
+    {"TemporalAffine", "{benchIdx=14;benchName='TemporalAff';benchN=100;inputShape=[100,80];N=100;M=256}"},
+    {"WordEmbedding", "{benchIdx=15;benchName='WordEmbed';benchN=100;inputShape=[100];D=100;V=256}"},
+    {"Svm", "{benchIdx=16;benchName='SVM';benchN=100;inputShape=[1024]}"},
+    {"Softmax", "{benchIdx=17;benchName='Softmax';benchN=100;inputShape=[1024]}"},
+    {"TemporalSoftmax", "{benchIdx=18;benchName='TemporalSM';benchN=100;inputShape=[100,80]}"},
+    {"TwoLayerNet", "{benchIdx=19;benchName='TwoLayerNet';benchN=100;inputShape=[1024];hidden=[1024,1024]}"},
 };
 
 MatrixN benchMean;
@@ -54,7 +59,7 @@ float updateMean(int y,int x,float f) {
     return benchMean(y,x);
 }
 
-bool benchLayer(string name, Layer* player, MatrixN &X, MatrixN &y, int row) {
+bool benchLayer(string name, Layer* player, MatrixN &X, MatrixN &y, int row0) {
     Timer tcpu;
     float tcus, tcusn, tfn, tf,tb, tfx, tbx;
     t_cppl cache;
@@ -62,6 +67,7 @@ bool benchLayer(string name, Layer* player, MatrixN &X, MatrixN &y, int row) {
     t_cppl states;
     MatrixN ya;
 
+    int row=row0+1;
     int N=X.rows();
     states["y"] = new MatrixN(y);
 
@@ -74,22 +80,22 @@ bool benchLayer(string name, Layer* player, MatrixN &X, MatrixN &y, int row) {
     ya=player->forward(X,&cache,&states,0);
     tcus=tcpu.stopCpuMicro()/1000.0;
     if (tcus<tf) tf=tcus;
-    tf=updateMean(row-1,0,tf);
+    tf=updateMean(row0,0,tf);
 
     tcpu.startCpu();
     player->backward(ya,&cache, &states, &grads, 0);
     tcus=tcpu.stopCpuMicro()/1000.0;
     if (tcus<tb) tb=tcus;
-    tb=updateMean(row-1,1,tb);
+    tb=updateMean(row0,1,tb);
 
     cppl_delete(&cache);
     cppl_delete(&grads);
     cppl_delete(&states);
 
     tfx= tf / (double)N;
-    tfx=updateMean(row-1,2,tfx);
+    tfx=updateMean(row0,2,tfx);
     tbx= tb / (double)N;
-    tbx=updateMean(row-1,3,tbx);
+    tbx=updateMean(row0,3,tbx);
 
     move(row,17); printw("%8.4f", tf);
     move(row,24); printw("%8.4f", tfx);
@@ -108,65 +114,76 @@ int doBench() {
     int mreps=100;
     benchMean=MatrixN(benchRecipes.size(),8);
     benchMean.setZero();
+    int maxrow=0;
     clear();
     for (int mrep=0; mrep<mreps; mrep++) {
-        int row=0;
         move(0,0); printw("Layer             fw(ms) fw/N(ms) bw(ms) bw/N(ms)");
         for (auto it : benchRecipes) {
-            ++row;
-            move(row,0); printw(it.first.c_str());
-            t_layer_props_entry te=_syncogniteLayerFactory.mapprops[it.first];
-            if (benchRecipes.find(it.first)==benchRecipes.end()) {
-                move(row, 24); printw("No bench recipe for layer %s", it.first.c_str());
-            } else {
-                CpParams cp(benchRecipes[it.first]);
-                int bs=cp.getPar("benchN",(int)0);
-                if (bs==0) {
-                    cerr << "No benchN batch size defined in recipe for layer " << it.first << endl;
-                    continue;
-                }
-                string bd=cp.getPar("benchDataType",(string)"");
-                vector<int> isv=cp.getPar("inputShape", vector<int>{});
-                if (isv.size()==0) {
-                    cerr << "No inputShape batch size defined in recipe for layer " << it.first << endl;
-                    continue;
-                }
-                int is=1;
-                for (auto k=0; k<isv.size(); k++) is *= isv[k];
-                MatrixN X(bs,is);
-                MatrixN y(bs,1);
-                if (bd=="") {
-                    X.setRandom();
-                } else if (bd=="int100") {
-                    for (auto k=0; k<X.size(); k++) {
-                        X(k)=rand()%100;
-                    }
-                } else {
-                    cerr << "No benchDataType: " << bd << " is not understood in recipe for layer " << it.first << endl;
-                    continue;
-                }
-                cp.setPar("train", true);
-                Layer *pl = CREATE_LAYER(it.first, cp)
-                if (pl->layerType & LayerType::LT_LOSS) {
-                    for (auto k=0; k<y.size(); k++) {
-                        y(k)=rand()%is;
-                    }
-                }
-                if (!benchLayer(it.first, pl, X, y, row)) {
-                    cerr << "Error" << endl;
-                    allOk=false;
-                }
-                delete pl;
+            string classname=it.first;
+            while (isdigit(classname[classname.size()-1])) {
+                classname=classname.substr(0,classname.size()-1);
             }
+            CpParams cp(benchRecipes[it.first]);
+            int row=cp.getPar("benchIdx",(int)-1);
+            if (row>=benchRecipes.size()) {
+                cerr << "benchIdx must not be equal or greater than size of benchRecipes, error in: " << it.first << endl;
+                exit(-1);
+            }
+            if (row==-1) {
+                cerr << "No benchIdx defined for: " << it.first << endl;
+                exit(-1);
+            }
+            string name=cp.getPar("benchName",it.first);
+            move(row+1,0); printw(name.c_str());
+            if (row+1 > maxrow) maxrow=row+1;
+            t_layer_props_entry te=_syncogniteLayerFactory.mapprops[classname];
+            int bs=cp.getPar("benchN",(int)0);
+            if (bs==0) {
+                cerr << "No benchN batch size defined in recipe for layer " << it.first << endl;
+                continue;
+            }
+            string bd=cp.getPar("benchDataType",(string)"");
+            vector<int> isv=cp.getPar("inputShape", vector<int>{});
+            if (isv.size()==0) {
+                cerr << "No inputShape batch size defined in recipe for layer " << it.first << endl;
+                continue;
+            }
+            int is=1;
+            for (auto k=0; k<isv.size(); k++) is *= isv[k];
+            MatrixN X(bs,is);
+            MatrixN y;
+            if (bd=="") {
+                X.setRandom();
+            } else if (bd=="int100") {
+                for (auto k=0; k<X.size(); k++) {
+                    X(k)=rand()%100;
+                }
+            } else {
+                cerr << "No benchDataType: " << bd << " is not understood in recipe for layer " << it.first << endl;
+                continue;
+            }
+            cp.setPar("train", true);
+            Layer *pl = CREATE_LAYER(classname, cp)
+            if (pl->layerType & LayerType::LT_LOSS) {
+                y=MatrixN(bs,pl->outputShape[0]);
+                for (auto k=0; k<y.size(); k++) {
+                    y(k)=rand()%isv[0];
+                }
+            }
+            if (!benchLayer(classname, pl, X, y, row)) {
+                cerr << "Error" << endl;
+                allOk=false;
+            }
+            delete pl;
         }
-        move(row+1,0);
         refresh();
         if (mrep<mreps/1.5) {
             DM += 0.5;
         }
+        int c=getch();
+        if (c=='q') break;
     }
-    printw("Press enter...");
-    getch();
+    move(maxrow+1,0);
     endwin();
     return allOk;
 }
@@ -226,7 +243,12 @@ void icolmagic() {
 
 
 int main() {
-    initscr();
+    initscr(); // curses
+    cbreak(); // no character buffering
+    noecho(); // no echo
+    nodelay(stdscr, TRUE); // async keyboard checks
+    // scrollok(stdscr, TRUE); // if scrolling is needed.
+
     cpInitCompute("Bench",nullptr,0);
     registerLayers();
     int ret=0;
