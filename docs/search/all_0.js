9afc6f6e7b5c6b01b9cd73e6d18c066139ecf0a3
Dominik Schlösser <dsc@dosc.net>
2022-03-23 08:54:51 +0100

    Start of doxygen doc

---
+++ docs/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['syncognite_20_2d_20a_20neural_20network_20library_20inspired_20by_20stanford_27s_20cs231n_20course_0',['syncognite - A neural network library inspired by Stanford&apos;s CS231n course',['../md__r_e_a_d_m_e.html',1,'']]]
+];
