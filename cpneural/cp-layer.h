740e7f6e0a814429cdd87d22d9a42385838c2397
Dominik Schlösser <dsc@dosc.net>
2022-04-01 10:21:10 +0200

    updateOptimizerParameter only updates parameters given, and keeps other params usingn new nlohmann-json -- requires test

--- cpneural/cp-layer.h
+++ cpneural/cp-layer.h
@@ -85,7 +85,12 @@ class Optimizer {
 public:
     json j;
     virtual ~Optimizer() {}; // Otherwise destructor of derived classes is never called!
-    virtual void updateOptimizerParameters(const json &opt_params) {j=opt_params;};
+    virtual void updateOptimizerParameters(const json &opt_params) {
+        for (auto& el : opt_params.items()) {
+            j[el.key()] = el.value();
+        }
+        //j = opt_params;
+    };
     virtual MatrixN update(MatrixN& x, MatrixN& dx, string var, t_cppl *pcache) {return x;};
 };
 
